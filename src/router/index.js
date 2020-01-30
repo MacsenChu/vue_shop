abf6a01e937389736b9591fa5b9177b9480d6c42
Macsen <7434311@qq.com>
2020-01-30 22:11:50 +0800

    完成了报表功能的开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params'
 import GoodsList from '../components/goods/List'
 import Add from '../components/goods/Add'
 import Order from '../components/order/Order'
+import Report from '../components/report/Report'
 
 Vue.use(VueRouter)
 
@@ -32,6 +33,7 @@ const routes = [
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: Add },
       { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
     ]
   }
 ]
